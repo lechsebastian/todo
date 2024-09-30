1a0fe8df1e7be240a4a680c976d33db70da82bdd
Sebastian Lech <lechsebastian99@gmail.com>
2024-09-30 23:03:06 +0200

    Add BottomNavigationBar to HomePage

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
     required this.user,
@@ -10,26 +11,73 @@ class HomePage extends StatelessWidget {
   final User user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('You are signed in as ${user.email}'),
-            const SizedBox(height: 20),
-            IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              
-              icon: const Icon(Icons.logout),
-              color: Colors.black,
-              iconSize: 30,
-            ),
-          ],
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return const Center(
+            child: Text('Tasks'),
+          );
+        }
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('You are signed in as ${widget.user.email}'),
+              const SizedBox(height: 20),
+              IconButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                icon: const Icon(Icons.logout),
+                color: Colors.black,
+                iconSize: 30,
+              ),
+            ],
+          ),
+        );
+      }),
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 20,
+          ),
+          child: GNav(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.grey.shade800,
+            padding: const EdgeInsets.all(16),
+            gap: 8,
+            onTabChange: (value) {
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            tabs: const [
+              GButton(
+                icon: Icons.assignment,
+                text: 'Tasks',
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: 'Settings',
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
