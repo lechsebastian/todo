948ae076ad57474e938888a7a8ecf6cd6254b94f
Sebastian Lech <lechsebastian99@gmail.com>
2024-11-27 23:01:23 +0100

    Create NoteModel

---
+++ lib/app/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String id;
+  final String title;
+  final String description;
+
+  NoteModel({
+    required this.id,
+    required this.title,
+    required this.description,
+  });
+}
